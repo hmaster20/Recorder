020b0e1b95505fb5fb220805b61c5ec46778c5a9
hmaster <hmaster20@gmail.com>
2017-07-16 22:02:12 +0300

    first

---
+++ Recorder/main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Recorder
+{
+    partial class main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(52, 211);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(322, 20);
+            this.FileName.TabIndex = 0;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(380, 209);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(27, 23);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(52, 25);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(355, 18);
+            this.info.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 259);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.FileName);
+            this.Name = "main";
+            this.Text = "Запись с микрофона..";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Label info;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Recorder/main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recorder
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+
+        public void Button1_Click(System.Object sender, System.EventArgs e)
+        {
+            record("open new Type waveaudio Alias recsound", "", 0, 0);
+
+            record("record recsound", "", 0, 0);
+        }
+
+        public void Button2_Click(System.Object sender, System.EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FileName.Text))
+            {
+                //record("save recsound c:\\mic.wav", "", 0, 0);
+                record("save recsound c:\\mic.wav", "", 0, 0);
+                record("close recsound", "", 0, 0);
+            }
+            else
+            {
+                return;
+            }
+
+
+        }
+
+        public void Button3_Click(System.Object sender, System.EventArgs e)
+        {
+            (new Microsoft.VisualBasic.Devices.Audio()).Play("c:\\mic.wav");
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = @"wav files|*.wav",
+                Title = @"Сохранить файл как",
+                DefaultExt = "wav",
+                OverwritePrompt = false
+
+
+                //Filter = @"mp3 files|*.mp3",
+                //Title = @"Save File As",
+                //DefaultExt = "mp3",
+                //OverwritePrompt = false
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName.Text = saveFileDialog.FileName;
+            }
+        }
+
+
+        //#region Default Instance
+
+        //private static Form1 defaultInstance;
+
+        //public static Form1 Default
+        //{
+        //    get
+        //    {
+        //        if (defaultInstance == null)
+        //        {
+        //            defaultInstance = new Form1();
+        //            defaultInstance.FormClosed += new FormClosedEventHandler(defaultInstance_FormClosed);
+        //        }
+        //        return defaultInstance;
+        //    }
+        //}
+
+        //static void defaultInstance_FormClosed(object sender, FormClosedEventArgs e)
+        //{
+        //    defaultInstance = null;
+        //}
+
+        //#endregion
+    }
+}
