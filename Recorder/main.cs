f173bef3feb43a3a7c6f114cfcd3736960230341
Sergei <hmaster20@gmail.com>
2017-07-17 14:44:07 +0300

    добавлены таймеры

--- Recorder/main.cs
+++ Recorder/main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -20,7 +21,19 @@ namespace Recorder
             btnPlay.Enabled = true;
             btnRec.Enabled = true;
             btnStop.Enabled = false;
+
+            timer.Tick += Timer_Tick;
+            info.Text = "00:00:00.0";
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = sWatch.Elapsed;
+            info.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+        }
+
+        Stopwatch sWatch = new Stopwatch();
 
         [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
         private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
@@ -38,6 +51,9 @@ namespace Recorder
                 btnStop.Enabled = true;
 
                 FileName.Enabled = false;
+
+                sWatch.Start();
+                timer.Start();
             }
             else
             {
@@ -52,6 +68,10 @@ namespace Recorder
                 //record("save recsound c:\\mic.wav", "", 0, 0);
                 record("save recsound " + FileName.Text, "", 0, 0);
                 record("close recsound", "", 0, 0);
+
+                sWatch.Start();
+                sWatch.Reset();
+                timer.Stop();
             }
             else
             {
